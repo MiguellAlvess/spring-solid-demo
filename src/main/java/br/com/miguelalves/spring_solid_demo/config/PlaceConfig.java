ab775c60fb29dfebe4144ae0f1694ec96fdaf482
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-13 12:39:19 -0300

    refactor. ensure that dates are filled in automatically

--- src/main/java/br/com/miguelalves/spring_solid_demo/config/PlaceConfig.java
+++ src/main/java/br/com/miguelalves/spring_solid_demo/config/PlaceConfig.java
@@ -2,11 +2,13 @@ package br.com.miguelalves.spring_solid_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 import br.com.miguelalves.spring_solid_demo.repository.PlaceRepository;
 import br.com.miguelalves.spring_solid_demo.service.PlaceService;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
 
     @Bean
--- src/main/java/br/com/miguelalves/spring_solid_demo/domain/Place.java
+++ src/main/java/br/com/miguelalves/spring_solid_demo/domain/Place.java
@@ -2,6 +2,10 @@ package br.com.miguelalves.spring_solid_demo.domain;
 
 import java.time.LocalDateTime;
 
-public record Place(Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+public record Place(Long id, String name, String slug, String state, @CreatedDate LocalDateTime createdAt,
+        @LastModifiedDate LocalDateTime updatedAt) {
 
 }
