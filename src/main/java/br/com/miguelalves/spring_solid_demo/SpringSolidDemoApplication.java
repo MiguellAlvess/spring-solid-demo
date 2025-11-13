0604869ccbc859f1ddf6429ea129899119dfab33
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-13 11:18:40 -0300

    chore: add initial project setup

---
+++ src/main/java/br/com/miguelalves/spring_solid_demo/SpringSolidDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.miguelalves.spring_solid_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSolidDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSolidDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/miguelalves/spring_solid_demo/SpringSolidDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.miguelalves.spring_solid_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSolidDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
