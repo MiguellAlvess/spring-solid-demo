228f13366426a0aed6ecb70909979487496ac8fd
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-13 13:00:40 -0300

    test: ensure place can be created successfully

--- src/test/java/br/com/miguelalves/spring_solid_demo/SpringSolidDemoApplicationTests.java
+++ src/test/java/br/com/miguelalves/spring_solid_demo/SpringSolidDemoApplicationTests.java
@@ -1,13 +1,38 @@
 package br.com.miguelalves.spring_solid_demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import br.com.miguelalves.spring_solid_demo.api.PlaceRequest;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SpringSolidDemoApplicationTests {
 
+	@Autowired
+	WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	public void shouldCreatePlaceSuccessfully() {
+		var name = "Valid name";
+		var state = "Valid state";
+		var slug = "valid-name";
+
+		webTestClient
+				.post()
+				.uri("/places")
+				.bodyValue(
+						new PlaceRequest(name, state))
+				.exchange()
+				.expectBody()
+				.jsonPath("name").isEqualTo(name)
+				.jsonPath("state").isEqualTo(state)
+				.jsonPath("slug").isEqualTo(slug)
+				.jsonPath("createdAt").isNotEmpty()
+				.jsonPath("updatedAt").isNotEmpty();
+
 	}
 
 }
