1738addbc2e92dc9cfcf2179ce120c159045f709
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-13 12:49:33 -0300

    fix: initialize Slugify instance in PlaceService and update slug generation

--- src/main/java/br/com/miguelalves/spring_solid_demo/service/PlaceService.java
+++ src/main/java/br/com/miguelalves/spring_solid_demo/service/PlaceService.java
@@ -3,17 +3,20 @@ package br.com.miguelalves.spring_solid_demo.service;
 import br.com.miguelalves.spring_solid_demo.api.PlaceRequest;
 import br.com.miguelalves.spring_solid_demo.domain.Place;
 import br.com.miguelalves.spring_solid_demo.repository.PlaceRepository;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest) {
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(),
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.slug()), placeRequest.state(),
                 placeRequest.createdAt(), placeRequest.updatedAt());
         return placeRepository.save(place);
     }
